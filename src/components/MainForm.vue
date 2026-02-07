e77b09c8d9a36002afb0dd97cee638a0edd63d83
Dmitry Romadov <d.romadov@fortus.pro>
2026-02-07 21:52:28 +0300

    (feat) Заготовки компонентов и подкчлюение vuetify

---
+++ src/components/MainForm.vue
@@ -0,0 +1,13 @@
+<template>
+  Здесь будет форма
+</template>
+
+<script setup lang="ts">
+
+
+
+</script>
+
+<style scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
 import App from './App.vue'
 
 const app = createApp(App)
+const pinia = createPinia()
+const vuetify = createVuetify()
 
-app.use(createPinia())
+app.use(pinia)
+app.use(vuetify)
 
 app.mount('#app')
